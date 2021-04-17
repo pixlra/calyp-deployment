e2b35ea1646f4d5bb077bf5cba52e99e50ef8740
Joao Carreira <jfmcarreira@gmail.com>
2021-04-17 02:03:07 +0100

    Remove wrong comment code in bat file

--- scripts/extract_ffmpeg.bat
+++ scripts/extract_ffmpeg.bat
@@ -1,5 +1,4 @@
 dir /P /B "ffmpeg" > tmp_filename_ffmpeg
 set /p ffmpeg_folder= < tmp_filename_ffmpeg
 mv "ffmpeg/%ffmpeg_folder%" ffmpeg_libraries
-# need to clean .exe files so we can then only install .dlls
 rm C:\Projects\ffmpeg_libraries\bin\*.exe
